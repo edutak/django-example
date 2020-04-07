f008d8f533c58e1c0b488b2a5bd78175ba516fd8
aclass <edutak.ssafy@gmail.com>
2020-04-07 01:24:35 +0000

    05 | Article Index

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', )
+
+# 어드민 사이트에 등록해줘
+admin.site.register(Article, ArticleAdmin)
