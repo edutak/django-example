f59498514b6001a6dc1e723470c831f09f762c42
aclass <edutak.ssafy@gmail.com>
2020-04-07 06:44:37 +0000

    Styling with bootstrap, django-bootstrap4

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Django
 
 > 앞으로 기본 CRUD 로직은 지금 작성된 구조로 균일하게 작성 예정입니다.
+
+## 추가 라이브러리
+
+* [django-bootstrap4](https://django-bootstrap4.readthedocs.io/en/latest/index.html)
+
+* 활용하기 위해서 반드시 설치를 진행하셔야 합니다.
+* 해당 문서를 보고 직접 설치, 등록을 해보세요 :)
--- articles/forms.py
+++ articles/forms.py
@@ -2,6 +2,27 @@ from django import forms
 from .models import Article
 
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+                max_length=100,
+                label='제목',
+                help_text='제목은 100자이내로 작성하세요.',
+                widget=forms.TextInput(
+                        attrs={
+                            'class': 'my-input',
+                            'placeholder': '제목 입력'
+                        }
+                    )
+            )
+    content = forms.CharField(
+                label='내용',
+                help_text='자유롭게 작성해주세요.',
+                widget=forms.Textarea(
+                        attrs={
+                            'row': 5,
+                            'col': 50,
+                        }
+                    )
+            )
     class Meta:
         model = Article
         fields = ['title', 'content']
