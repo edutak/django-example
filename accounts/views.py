42ff09546ea7f3f051d5646af4c3cdd360bde55c
aclass <edutak.ssafy@gmail.com>
2020-04-13 06:11:23 +0000

    Accounts02 | detail page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
@@ -15,3 +17,11 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def detail(request, pk):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/detail.html', context)
