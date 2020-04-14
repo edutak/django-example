48a9344c2bc70d22f6858e4e59c3c7cff810c025
aclass <edutak.ssafy@gmail.com>
2020-04-14 05:45:50 +0000

    Accounts06 | login_required

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-# from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'first_name', 'last_name', 'email']
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # from .forms import CustomUserChangeForm
@@ -9,6 +10,10 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup(request):
+    # 로그인이 되어있다면,
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -31,6 +36,8 @@ def detail(request, pk):
     return render(request, 'accounts/detail.html', context)
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
     if request.method == 'POST':
         # 사용자가 보낸 값 -> form
         form = AuthenticationForm(request, request.POST)
@@ -38,7 +45,7 @@ def login(request):
         if form.is_valid():
             # 검증 완료시 로그인!
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+            return redirect(request.GET.get('next') or 'articles:index')
     else:
         form = AuthenticationForm()
     context = {
@@ -46,6 +53,8 @@ def login(request):
     }
     return render(request, 'accounts/login.html', context)
 
+@login_required
 def logout(request):
+    # 조건식으로 직접 작성 해도 된다.
     auth_logout(request)
     return redirect('articles:index')
