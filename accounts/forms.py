9dcf2cd3f1776db84bf484e0e1c50ac8070febcc
aclass <edutak.ssafy@gmail.com>
2020-04-28 05:48:38 +0000

    User custom

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,14 @@
 # from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 # 그대로 활용하지 못하는 경우는 항상 상속받아서 custom!!!!
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'first_name', 'last_name', 'email']
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
